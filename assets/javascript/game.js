60799371a9d8d499c7839c97fdf9c7461ec8d25b
Chelsea Laura <cdesrosiers419@gmail.com>
2018-12-20 16:24:57 -0500

    Wins and losses update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,7 +5,12 @@ $(document).ready(function() {
 
   $("#number-to-guess").text(targetNumber);
 
+  var wins = 0;
+  var losses = 0;
   var counter = 0;
+
+  var winsText = document.getElementById("wins-text");
+  var lossesText = document.getElementById("losses-text");
 
 
   var numberOptions = [1, 5, 7, 3];
@@ -30,12 +35,17 @@ $(document).ready(function() {
     $("#your-score").text(counter);
 
     if (counter === targetNumber) {
+      wins++;
       alert("You Won!");
     }
 
     else if (counter >= targetNumber) {
+      losses++;
       alert("You Lose!");
     }
+
+    winsText.textContent = "Wins: " + wins;
+    lossesText.textContent = "Losses: " + losses;
 
   });
 
